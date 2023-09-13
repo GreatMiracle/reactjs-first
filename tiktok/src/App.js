c1f3165964488f0c1acdb630f260e9a570c19674
kien-sotatek <kiennguyen16998@gmail.com>
2023-09-13 17:20:54 +0700

    end useReducer 44

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useReducer, useRef, useState } from 'react';
 import Content1 from './Content';
 import React from 'react';
 
@@ -185,15 +185,218 @@ import React from 'react';
 //   );
 // }
 // ============================================================================
-function App() {
-  const [show, setShow] = useState(false);
+// function App() {
+//   const [show, setShow] = useState(false);
 
+//   return (
+//     <div style={{ padding: 32 }}>
+//       <button onClick={() => setShow(!show)}>Todo</button>
+//       {show && <Content1 />}
+//     </div>
+//   );
+// }
+// ============================================================================
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   const handleIncrease = useCallback(() => {
+//     setCount(count + 1)}, [])
+//   ;
+
+//   return (
+//     <div style={{ padding: 20 }}>
+//       <Content1 onIncrease ={handleIncrease}/>
+//       <h1>{count}</h1>
+
+//     </div>
+//   );
+// }
+// ============================================================================
+
+// function App() {
+//   const [name, setName] = useState('');
+//   const [price, setPrice] = useState('');
+//   const [products, setProducts] = useState([]);
+
+//   const nameRef = useRef();
+
+//   const handleSubmit = () => {
+//     setProducts([
+//       ...products,
+//       {
+//         name,
+//         price: parseInt(price),
+//       },
+//     ]);
+
+//     setName('');
+//     setPrice('');
+//     nameRef.current.focus();
+//   };
+
+//   const total = useMemo(() => {
+//     const resultsTotal = products.reduce((accumulator, currentValue) => {
+//       console.log('caculating...');
+//       return accumulator + currentValue.price;
+//     }, 0);
+//     return resultsTotal;
+//   }, [products]);
+
+//   return (
+//     <div style={{ padding: '10px 32px' }}>
+//       <input
+//         ref={nameRef}
+//         value={name}
+//         placeholder="Enter name..."
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <br />
+//       <input
+//         value={price}
+//         placeholder="Enter price..."
+//         onChange={(e) => setPrice(e.target.value)}
+//       />
+//       <br />
+//       <button onClick={handleSubmit}>Add</button>
+//       <br />
+//       <span>Total:</span>
+//       <ul>
+//         {products.map((product, index) => (
+//           <li key={index}>
+//             {product.name} -{product.price}
+//           </li>
+//         ))}
+//         {products && <li>-----------------------</li>}
+//         {total || <li>{total}</li>}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// ============================================================================
+// const initState = 0;
+// const UP_ACTION = 'up';
+// const DOWN_ACTION = 'down';
+
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case UP_ACTION:
+//       return state + 1;
+//     case DOWN_ACTION:
+//       return state - 1;
+//     default:
+//       throw new Error('Invalid.....');
+//   }
+// };
+
+// function App() {
+//   const [count, dispatch] = useReducer(reducer, initState);
+
+//   return (
+//     <div style={{ padding: '0 20px' }}>
+//       <h1>{count}</h1>
+//       <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+//       <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+//     </div>
+//   );
+// }
+
+// ============================================================================
+
+const initState = {
+  job: '',
+  jobs: [],
+};
+const SET_ACTION = 'set';
+const ADD_ACTION = 'add';
+const REMOVE_ACTION = 'remove';
+
+const setJob = (payload) => {
+  return {
+    type: SET_ACTION,
+    payload,
+  };
+};
+
+const addJob = (payload) => {
+  return {
+    type: ADD_ACTION,
+    payload,
+  };
+};
+
+const deleteJob = (payload) => {
+  return {
+    type: REMOVE_ACTION,
+    payload,
+  };
+};
+
+const reducer = (state, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_ACTION:
+      newState = {
+        ...state,
+        job: action.payload,
+      };
+      break;
+    case ADD_ACTION:
+      newState = {
+        ...state,
+        jobs: [...state.jobs, action.payload],
+      };
+
+      break;
+
+    case REMOVE_ACTION:
+      const jobsAfferRemove = state.jobs.filter(
+        (element, index) => index !== action.payload
+      );
+      newState = {
+        ...state,
+        jobs: [...jobsAfferRemove],
+      };
+
+      break;
+    default:
+      throw new Error('Invalid...');
+  }
+  return newState;
+};
+
+function App() {
+  const [todo, dispatch] = useReducer(reducer, initState);
+
+  const handleSubmit = () => {
+    dispatch(addJob(todo.job));
+    dispatch(setJob(''));
+  };
+
+  // console.log(todo.job);
   return (
-    <div style={{ padding: 32 }}>
-      <button onClick={() => setShow(!show)}>Todo</button>
-      {show && <Content1 />}
+    <div style={{ padding: '0 20px' }}>
+      <h3>Todo</h3>
+      <input
+        type="text"
+        value={todo.job}
+        placeholder="Enter todo..."
+        onChange={(e) => dispatch(setJob(e.target.value))}
+      ></input>
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {todo.jobs.map((item, index) => {
+          return (
+            <li key={index}>
+              {item}
+
+              <span onClick={() => dispatch(deleteJob(index))}> x</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
 export default App;
--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState, memo } from 'react';
 import React from 'react';
 
-const tab = ['posts', 'todos', 'albums'];
+// const tab = ['posts', 'todos', 'albums'];
 
 // function Content(mapping) {
 //   const [todo, setTodo] = useState([]);
@@ -65,24 +65,213 @@ const tab = ['posts', 'todos', 'albums'];
 //     </div>
 //   );
 // }
+// ========================================
+// hiển thị độ rộng của browser
+// function Content() {
+//   const [withChrom, setWithChrom] = useState(window.innerWidth);
 
-function Content() {
-  const [withChrom, setWithChrom] = useState(window.innerWidth);
+//   useEffect(() => {
+//     const handleResize = ()=> {
+//       setWithChrom(window.innerWidth)
+//     }
 
-  useEffect(() => {
-    const handleResize = ()=> {
-      setWithChrom(window.innerWidth)
-    }
+//     window.addEventListener('resize', handleResize )
 
-    window.addEventListener('resize', handleResize )
+//   }, [])
 
-  }, [])
+//   return (
+//     <div>
+//       <h1>{withChrom}</h1>
+//     </div>
+//   );
+// }
 
+// ========================================
+// function Content() {
+//   const [countDown, setCountDown] = useState(180);
+
+//   // useEffect(() => {
+//   //   setTimeout(() => setCountDown(countDown - 1), 1000)
+//   // })
+
+//   useEffect(() => {
+//     const timerId = setInterval(() => {
+//       setCountDown((prevState) => prevState - 1);
+//       console.log("countDown");
+//     }, 1000);
+
+//     return ()=>clearInterval(timerId);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>{countDown}</h1>
+//     </div>
+//   );
+// }
+
+// ========================================
+
+// function Content() {
+//   const [count, setCount] = useState(1);
+
+//   // useEffect(() => {
+//   //   setTimeout(() => setCountDown(countDown - 1), 1000)
+//   // })
+
+//   useEffect(() => {
+//     console.log(count);
+
+//     return () => console.log("clean");;
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Click me!</button>
+//     </div>
+//   );
+// }
+
+// ========================================
+
+// function Content() {
+//   const [avatar, setAvatar] = useState();
+
+//   // useEffect(() => {
+//   //   return () => avatar && URL.revokeObjectURL(avatar.preview);
+//   // }, [avatar]);
+
+//   const handlePreviewAvatar = (e) => {
+//     const file = e.target.files[0];
+//     file.preview = URL.createObjectURL(file);
+//     console.dir(file);
+//     setAvatar(file);
+//   };
+
+//   return (
+//     <div>
+//       <input type="file" onChange={handlePreviewAvatar} />
+//       {avatar && <img src={avatar.preview} alt="" width="80px"></img>}
+//     </div>
+//   );
+// }
+
+// ========================================
+
+// const lessions = [
+//   {
+//     id: 1,
+//     name: 'Bài 1',
+//   },
+//   {
+//     id: 2,
+//     name: 'Bài 2',
+//   },
+//   {
+//     id: 3,
+//     name: 'Bài 3',
+//   },
+// ];
+
+// function Content() {
+//   const [lessionId, setLessionId] = useState(1);
+//   const [first, setfirst] = useState("second")
+
+//   useEffect(() => {
+//     const cusEvent = (e) => {
+//       console.log(e.detail);
+//     };
+
+//     window.addEventListener(`ipv-${lessionId}`, cusEvent);
+
+//     return() => window.removeEventListener(`ipv-${lessionId}`, cusEvent);
+//   }, [lessionId]);
+
+//   return (
+//     <div>
+//       <ul>
+//         {lessions.map((l) => (
+//           <li
+//             key={l.id}
+//             style={{
+//               color: lessionId === l.id ? 'red' : '#333',
+//             }}
+//             onClick={() => setLessionId(l.id)}
+//           >
+//             {l.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// ========================================
+
+// function Content() {
+//   const [count, setCount] = useState(1);
+
+//   useLayoutEffect(() => {
+//     if (count > 3) {
+//       setCount(0);
+//     }
+//     console.log(count);
+
+//     return () => console.log('clean');
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Click me!</button>
+//     </div>
+//   );
+// }
+
+// ========================================
+
+// function Content() {
+//   const [count, setCount] = useState(60);
+
+//   const timerId = useRef(60);
+
+//   const handleStart = () => {
+//     timerId.current = setInterval(() => setCount(prev => prev- 1), 1000);
+
+//     console.log('start:' +timerId.current);
+
+//   };
+
+//   const handleStop = () => {
+//     clearInterval(timerId);
+//     setCount(60)
+//     console.log('stop:' +timerId.current);
+
+//   };
+
+//   return (
+//     <div style={{ padding: 20 }}>
+//       <h1>{count}</h1>
+//       <button onClick={handleStart}>Start</button>
+//       <button onClick={handleStop}>Stop</button>
+//     </div>
+//   );
+// }
+
+// ========================================
+
+function Content({ onIncrease }) {
+  console.log('memo');
   return (
-    <div>
-      <h1>{withChrom}</h1>
-    </div>
+    <>
+      <h2>ABC</h2>
+      <button onClick={onIncrease}>Click me!</button>
+    </>
   );
 }
 
-export default Content;
+export default memo(Content);
+
+// useLayoutEffect gần giống useEffect chỉ khác là nó render lên giao diện cuối cùng
+//useRef: lưu giá trị qua một tham chiếu bên ngoài
